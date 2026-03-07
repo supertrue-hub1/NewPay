bffd6b03d0d30066ddd914c3f96f294bca3eea1f
supertrue <supertrue@github.com>
2026-03-08 01:54:47 +0300

    fix: disable locale prefix in URLs

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -2,5 +2,6 @@ import { defineRouting } from 'next-intl/routing';
 
 export const routing = defineRouting({
   locales: ['ru', 'uz', 'tg', 'ky'],
-  defaultLocale: 'ru'
+  defaultLocale: 'ru',
+  localePrefix: 'never' // Отключить префикс локали в URL
 });
